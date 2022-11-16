36c0f8d499223d0c344967b43773be73c726e1dc
DemianFerreyra <demiancra@gmail.com>
2022-11-16 17:38:50 -0300

    Champion vote route added

--- src/routes/champions.js
+++ src/routes/champions.js
@@ -6,5 +6,17 @@ router.get("/", async (req, res) => {
     let AllChampions = await Champion.findAll();
     res.status(200).send(AllChampions);
 });
+router.post("/", async (req, res) => {
+    const { team } = req.body;
+
+    try {
+        await Champion.create({
+            team: team.toLowerCase()
+        })
+        res.status(200).send(`Has apostado que ${team} saldra campeon!`);
+    } catch (error) {
+        res.status(400).send("Algo ha salido mal :(");
+    }
+});
 
 module.exports = router;
