14e70fa4639a459ce0dfc7daedcf6d2624ba5130
Sergioandres08 <sergioandrestorrez@gmail.com>
2022-11-16 23:07:43 -0500

    Results route fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const router = Router();
 //import rutas
 const championsRouter = require("./champions.js");
+const resultsRouter = require("./results.js")
 
 router.use("/champions", championsRouter);
 router.use("/results", resultsRouter);
