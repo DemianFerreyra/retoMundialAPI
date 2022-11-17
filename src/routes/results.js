e9aae0f80ba29e35199b053e8f6204016a488bf4
Sergioandres08 <sergioandrestorrez@gmail.com>
2022-11-17 14:18:12 -0500

    Results route fix again

--- src/routes/results.js
+++ src/routes/results.js
@@ -1,15 +1,22 @@
 const { Router } = require("express");
 const router = Router();
-const { Result } = require("../db");
+const { Result, Game } = require("../db");
 
 router.post('/', async (req, res) => {
-   await Result.create({
-        userUbication: req.body.ubication,
+   let resultado = await Result.create({
+        userUbication: req.body.userUbication,
         team1Result: req.body.team1Result,
         team2Result: req.body.team2Result
 
     })
+    let id = req.body.id 
 
+    let game = await Game.findOne({
+        where: {id:id}
+        
+    })
+
+    await game.addResults(resultado)
     res.status(200);
 })
 module.exports = router;
